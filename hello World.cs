552b0ddab6e95d029738d15354fb97578bfb9ec5
Barkhayot Juraev <coderviewer@gmail.com>
2021-01-05 15:53:54 +0900

    files

---
+++ hello World.cs
@@ -0,0 +1,12 @@
+using system;
+
+namespace Hello World
+{   
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Writeline("Hello World")
+        }
+    }
+}
