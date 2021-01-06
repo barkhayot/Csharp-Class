9ea195ff4afae996afed0038378366308404066e
Barkhayot Juraev <coderviewer@gmail.com>
2021-01-07 00:31:08 +0900

    files

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] movies={"Harry Potter", "Secret Garden", "Narnia", "Lord of the Rings", "The Hobbit"};
+
+            for (int i = 0; i < movies.Length; i++)
+            {
+                int rank = i + 1;
+                Console.WriteLine(rank + ". " + movies[i]);
+            }
+        }
+    }
+}
