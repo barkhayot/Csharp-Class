fb95212a59bfecf581df455e93be07cf27c29e79
Barkhayot Juraev <coderviewer@gmail.com>
2021-01-14 20:51:17 +0900

    updation

--- Main.cs
+++ Main.cs
@@ -7,7 +7,7 @@ public class Main : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        // Card Edition for begining;
     }
 
     // Update is called once per frame
