6ba0f92f64cee3fb45a716faddd08d28c47e6cf4
Barkhayot Juraev <coderviewer@gmail.com>
2021-01-19 16:25:03 +0900

    commit

--- Main.cs
+++ Main.cs
@@ -4,10 +4,16 @@ using UnityEngine;
 
 public class Main : MonoBehaviour
 {
+
+    public ForDevCards;
+    public ForLenCards;
+    
     // Start is called before the first frame update
     void Start()
     {
         // Card Edition for begining;
+        
+        
     }
 
     // Update is called once per frame
