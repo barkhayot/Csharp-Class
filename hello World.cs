7763280bdaf04f96bc305bd864171f9216f4d6ca
Barkhayot Juraev <coderviewer@gmail.com>
2021-01-28 19:55:40 +0900

    commit

--- hello World.cs
+++ hello World.cs
@@ -1,9 +1,12 @@
 using system;
+using UnityEngine;
+using UnityEngine.SenceManagement;
 
 namespace Hello World
 {   
     class Program
     {
+        public void Forloading()
         static void Main(string[] args)
         {
             Console.Writeline("Hello World")
